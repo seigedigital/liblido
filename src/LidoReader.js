b3cafd32c08442004eb4e83d9a1a0f46b93367af
Leander Seige <leander@seige.name>
2022-01-06 06:42:42 +0100

    update

--- src/LidoReader.js
+++ src/LidoReader.js
@@ -6,8 +6,12 @@ const LidoRecord = require('./LidoRecord.js')
 class LidoReader {
 
   constructor(data) {
+    console.log("LR1")
     this.doc = new DOMParser().parseFromString(data)
+    console.log({doc:this.doc})
+    console.log("LR2")
     this.select = lidotools.useNamespaces()
+    console.log("LR3")
   }
 
   getAllRecords() {
